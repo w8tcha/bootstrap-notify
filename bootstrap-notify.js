6e0869dc311a3c42bd778f38de2b3e43da9408ff
Ingo <thewatcher@watchersnet.de>
2023-09-08 08:46:09 +0200

    added css file

--- bootstrap-notify.js
+++ bootstrap-notify.js
@@ -307,7 +307,7 @@ extend(Notify.prototype,
                 this.$ele.dataset.hover = "false";
             });
           
-            this.$ele.dataset.hover = "true";
+            this.$ele.dataset.hover = "false";
 
             if (this.settings.delay > 0) {
                 self.$ele.dataset.notifyDelay = self.settings.delay;
@@ -320,6 +320,7 @@ extend(Notify.prototype,
                         self.$ele.dataset.notifyDelay = delay;
 
                         if (self.settings.showProgressbar) {
+                          
                             const div = self.$ele.querySelector('[data-notify="progressbar"] > div');
 
                             self.$ele.querySelector('[data-notify="progressbar"]').setAttribute("aria-valuenow", percent);
@@ -349,3 +350,38 @@ extend(Notify.prototype,
             self.$ele.remove();
         }
     });
+
+function extend(a, b) {
+    for (let key in b)
+        if (b.hasOwnProperty(key))
+            a[key] = b[key];
+    return a;
+}
+
+function deepExtend(out, ...arguments_) {
+    if (!out) {
+        return {};
+    }
+
+    for (const obj of arguments_) {
+        if (!obj) {
+            continue;
+        }
+
+        for (const [key, value] of Object.entries(obj)) {
+            switch (Object.prototype.toString.call(value)) {
+            case "[object Object]":
+                out[key] = out[key] || {};
+                out[key] = deepExtend(out[key], value);
+                break;
+            case "[object Array]":
+                out[key] = deepExtend(new Array(value.length), value);
+                break;
+            default:
+                out[key] = value;
+            }
+        }
+    }
+
+    return out;
+}
